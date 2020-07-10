3e07870221ece57978dd2811539733dd71fb168a
yalmazhasan <yalmazhasanbutt@gmail.com>
2020-07-10 13:26:14 +0300

    otp-layout for all screen sizes

--- app/src/main/java/com/funkymaster/hasbni/ui/OTPActivity.kt
+++ app/src/main/java/com/funkymaster/hasbni/ui/OTPActivity.kt
@@ -1,10 +1,39 @@
 package com.funkymaster.hasbni.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.funkymaster.hasbni.R
 
-class OTPActivity : AppCompatActivity() {
+class OTPActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var  one_view :TextView
+    private lateinit var  cancel_view :ImageView
+    private lateinit var  two_view :TextView
+    private lateinit var  three_view :TextView
+    private lateinit var  four_view :TextView
+    private lateinit var  five_view :TextView
+    private lateinit var  six_view :TextView
+    private lateinit var  seven_view :TextView
+    private lateinit var  eight_view :TextView
+    private lateinit var  nine_view :TextView
+    private lateinit var  zero_view :TextView
+    private lateinit var otp_et1 :EditText
+    private lateinit var otp_et2 :EditText
+    private lateinit var otp_et3 :EditText
+    private lateinit var otp_et4 :EditText
+
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +42,140 @@ class OTPActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         } //catch
         setContentView(R.layout.activity_otp)
+        initialize_variables()
+    } //on create
+
+    fun initialize_variables(){
+        one_view = findViewById(R.id.one_view)
+        two_view = findViewById(R.id.two_view)
+        three_view = findViewById(R.id.three_view)
+        four_view = findViewById(R.id.four_view)
+        five_view = findViewById(R.id.five_view)
+        six_view = findViewById(R.id.six_view)
+        seven_view = findViewById(R.id.seven_view)
+        eight_view = findViewById(R.id.eight_view)
+        nine_view = findViewById(R.id.nine_view)
+        zero_view = findViewById(R.id.zero_view)
+        otp_et1 = findViewById(R.id.otp_et1)
+        otp_et2 = findViewById(R.id.otp_et2)
+        otp_et3 = findViewById(R.id.otp_et3)
+        otp_et4 = findViewById(R.id.otp_et4)
+        cancel_view = findViewById(R.id.cancel_view)
+        otp_et1.addTextChangedListener(GenericTextWatcher(otp_et1, otp_et2));
+        otp_et2.addTextChangedListener(GenericTextWatcher(otp_et2, otp_et3));
+        otp_et3.addTextChangedListener(GenericTextWatcher(otp_et3, otp_et4));
+        otp_et4.addTextChangedListener(GenericTextWatcher(otp_et4, null));
+
+        otp_et1.requestFocus() //setting default focus to first edit text
+        otp_et1.setInputType(InputType.TYPE_NULL); //hiding the keyboard
+        otp_et2.setInputType(InputType.TYPE_NULL);
+        otp_et3.setInputType(InputType.TYPE_NULL);
+        otp_et4.setInputType(InputType.TYPE_NULL);
     }
 
-}
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.one_view->{
+                set_value(1)
+            }
+            R.id.two_view->{
+                set_value(2)
+            }
+            R.id.three_view->{
+                set_value(3)
+            }
+            R.id.four_view->{
+                set_value(4)
+            }
+            R.id.five_view->{
+                set_value(5)
+            }
+            R.id.six_view->{
+                set_value(6)
+            }
+            R.id.seven_view->{
+                set_value(7)
+            }
+            R.id.eight_view->{
+                set_value(8)
+            }
+            R.id.nine_view->{
+                set_value(9)
+            }
+            R.id.zero_view->{
+                set_value(0)
+            }
+            R.id.cancel_view-> {
+                if (otp_et4.hasFocus()) {
+                    if (otp_et4.length() == 1) {
+                        //last edit text has value filled so delete it and keep the focus
+                        otp_et4.text = null
+                    } else {
+                        otp_et3.text = null
+                        otp_et3.requestFocus()
+                    }
+                } else if (otp_et3.hasFocus()) {
+                    if (otp_et3.length() == 1) {
+                        otp_et3.text = null
+                    } else {
+                        otp_et2.text = null
+                        otp_et3.requestFocus()
+                    }
+                } else if (otp_et2.hasFocus()) {
+                    if (otp_et2.length() == 1) {
+                        otp_et2.text = null
+                    } else {
+                        otp_et1.text = null
+                        otp_et1.requestFocus()
+                    }
+                } else {
+                        otp_et1.requestFocus()
+                        otp_et1.text = null
+                } //end of else if
+            }
+        } //where
+    } //on click
+
+    fun set_value(no: Int){
+        //this function will set value to the edit text according to the number pressed
+        if(otp_et1.hasFocus())
+            otp_et1.setText(no.toString())
+        else if(otp_et2.hasFocus())
+            otp_et2.setText(no.toString())
+        else if(otp_et3.hasFocus())
+            otp_et3.setText((no.toString()))
+        else if(otp_et4.hasFocus())
+            otp_et4.setText(no.toString())
+    }   //set value
+
+    class GenericTextWatcher internal constructor(private val currentView: View, private val nextView: View?) :
+        TextWatcher {
+        override fun afterTextChanged(editable: Editable) { // TODO Auto-generated method stub
+            val text = editable.toString()
+            when (currentView.id) {
+                R.id.otp_et1 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.otp_et2 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.otp_et3 -> if (text.length == 1) nextView!!.requestFocus()
+                //You can use EditText4 same as above to hide the keyboard
+            }
+        }
+
+        override fun beforeTextChanged(
+            arg0: CharSequence,
+            arg1: Int,
+            arg2: Int,
+            arg3: Int
+        ) { // TODO Auto-generated method stub
+        }
+
+        override fun onTextChanged(
+            arg0: CharSequence,
+            arg1: Int,
+            arg2: Int,
+            arg3: Int
+        ) { // TODO Auto-generated method stub
+        }
+
+    }
+
+}   //class otp activity
--- app/src/main/java/com/funkymaster/hasbni/ui/RegistrationActivity.kt
+++ app/src/main/java/com/funkymaster/hasbni/ui/RegistrationActivity.kt
@@ -1,12 +1,19 @@
 package com.funkymaster.hasbni.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.funkymaster.hasbni.R
+import com.funkymaster.hasbni.utilities.Util
 
 class RegistrationActivity : AppCompatActivity() {
 
-
+    private lateinit var emailReg_field : EditText
+    private lateinit var passwordReg_field : EditText
+    private lateinit var registration: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +23,29 @@ class RegistrationActivity : AppCompatActivity() {
         } //catch
         setContentView(R.layout.activity_registration)
         initialize_variables()
+        onClickWorkings()
     }   //on create method
 
 
     fun initialize_variables(){
-
+        emailReg_field = findViewById(R.id.emailReg_field)
+        passwordReg_field = findViewById(R.id.passwordReg_field)
+        registration = findViewById(R.id.registration_btn)
     }   //initialize variables
 
+    fun onClickWorkings(){
+        var email = emailReg_field.text.toString()
+        var password = passwordReg_field.text.toString()
+        if(email.equals("") || password.equals(""))
+            Toast.makeText(this, "Both email & password are required!" , Toast.LENGTH_LONG).show()
+        else{
+            if(!Util.isEmailValid(email))
+                Toast.makeText(this, "Please enter a valid email!" , Toast.LENGTH_LONG).show()
+            else{
+                //both email & password are correct proceed
+                startActivity(Intent(this, OTPActivity :: class.java))
+            } //inner else
+        }   //outer else
+    }   //on click
 
 }   //registration class
